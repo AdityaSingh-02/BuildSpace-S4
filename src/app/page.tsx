4a7c15973ccebdb8b414ffad766a0e79d17198f8
Aditya Singh <asaditya2002@gmail.com>
2023-08-19 00:51:40 +0530

    WIP: Adding Aut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-5xl p-6">
         <h1 className="underline text-gray-300">BuildSpace News Letter</h1>
-        <p className="text-lg text-teal-500">News Letter Service made easy</p>
+        <p className="text-lg text-teal-500 font-semibold ">
+          News Letter Service made easy
+        </p>
       </div>
       <div>
-        <button className="border-2 text-teal-500 p-4 rounded-xl">Get Started</button>
+        <button className="flex justify-center items-center border-2 text-teal-500 p-4 rounded-xl hover:text-cyan-500 hover:font-semibold hover:border-teal-500 duration-300 ease-in-out">
+          Get Started {<BsArrowRight />}
+        </button>
       </div>
     </main>
   );
