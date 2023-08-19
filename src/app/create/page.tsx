a328c65ebd48deb00c788555e76e5eee1bc986ce
Aditya Singh <asaditya2002@gmail.com>
2023-08-19 10:38:57 +0530

    Mobile view updt

---
+++ src/app/create/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <>
+      <main className='flex justify-center items-center'>
+        <nav className='flex justify-center space-x-9 pt-10 text-3xl'>
+          <div>
+            Log in
+          </div>
+          <div>
+            Sign Up
+          </div>
+        </nav>
+        <div>
+          Creating
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Create
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
+"use client"
 import { BsArrowRight } from "react-icons/bs";
+import {useRouter, } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const getStarted = () => {
+    router.push('/create')
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-5xl p-6">
+    <main className="flex min-h-screen flex-col items-center justify-center p-5 md:p-24">
+      <div className="text-2xl md:text-5xl p-6">
         <h1 className="underline text-gray-300">BuildSpace News Letter</h1>
-        <p className="text-lg text-teal-500 font-semibold ">
+        <p className="text-sm md:text-lg text-teal-500 font-semibold ">
           News Letter Service made easy
         </p>
       </div>
       <div>
-        <button className="flex justify-center items-center border-2 text-teal-500 p-4 rounded-xl hover:text-cyan-500 hover:font-semibold hover:border-teal-500 duration-300 ease-in-out">
+        <button onClick={getStarted} className="flex justify-center items-center border-2 text-teal-500 md:p-4 p-2 rounded-xl hover:text-cyan-500 hover:font-semibold hover:border-teal-500 duration-300 ease-in-out">
           Get Started {<BsArrowRight />}
         </button>
       </div>
