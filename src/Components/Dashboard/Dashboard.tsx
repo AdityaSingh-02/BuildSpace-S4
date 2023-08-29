a05824b97f578392ad0be5e77ecd03341625f6b3
Aditya Singh <asaditya2002@gmail.com>
2023-08-29 21:42:00 +0530

    � add: DashBoard component Data

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
     <>
-        <h1>dashboard</h1>
+      <div>
+        <h1 className="text-xl font-semibold">DashBoard</h1>
+        <div className=" grid grid-cols-4 border-2 border-gray-500 my-10 px-10 h-72 rounded-xl items-center ">
+          <div className="flex justify-center border-r-2 border-gray-500 h-full">1</div>
+          <div className="flex justify-center border-r-2 border-gray-500 h-full">2</div>
+          <div className="flex justify-center border-r-2 border-gray-500 h-full">3</div>
+          <div className="flex justify-center h-full">4</div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
