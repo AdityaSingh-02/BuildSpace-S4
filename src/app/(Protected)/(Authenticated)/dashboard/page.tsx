40ebd0c2aa4d866a3fe4da7245cde4a4c6f226cb
Aditya Singh <asaditya2002@gmail.com>
2023-08-29 07:59:12 +0530

    chore: Updating Dashboard UI

--- src/app/(Protected)/(Authenticated)/dashboard/page.tsx
+++ src/app/(Protected)/(Authenticated)/dashboard/page.tsx
@@ -1,23 +1,24 @@
 "use client";
 import React, { useEffect } from "react";
-import { useUserData } from "@/Context";
+import {BiSolidDashboard, BiAnalyse, BiMoney} from 'react-icons/bi'
+import {BsFillPencilFill} from 'react-icons/bs'
 
 const dashboard = () => {
-  const { userData } = useUserData();
-
-  useEffect(() => {
-    console.log(userData);
-  }, []);
-
   return (
     <>
       <div>
         <div className="flex flex-row w-screen h-screen">
-          <div className="w-64 bg-cyan-950">
-            <h1>Aditya's NewsLetter</h1>
-          </div>
-          <div className="w-screen bg-cyan-900">
+          <div className="w-screen bg-gray-950 p-10">
             <h1>DashBoard</h1>
+          </div>
+          <div className="w-96 bg-gray-900 px-5 py-5">
+            <h1>Aditya's NewsLetter</h1>
+            <div className="border-2 border-gray-700 w-[100%] h-96 rounded-lg flex flex-col items-center space-y-4 mt-6 py-4">
+              <button className="w-[90%] border-2 border-gray-600 py-2 rounded-lg flex justify-center flex-row"><BiSolidDashboard size={22} /> DashBoard</button>
+              <button className="w-[90%] border-2 border-gray-600 py-2 rounded-lg flex justify-center flex-row"><BsFillPencilFill size={18} /> Write</button>
+              <button className="w-[90%] border-2 border-gray-600 py-2 rounded-lg flex justify-center flex-row"><BiAnalyse size={22} /> Analyze</button>
+              <button className="w-[90%] border-2 border-gray-600 py-2 rounded-lg flex justify-center flex-row"><BiMoney size={22}/>Monetization</button>
+            </div>
           </div>
         </div>
       </div>
