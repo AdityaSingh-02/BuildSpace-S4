370be7fd02f7cc6b4bffc1199244188fd571c3be
Aditya Singh <asaditya2002@gmail.com>
2023-09-01 22:17:18 +0530

    design: layout-Compose

--- src/app/(Protected)/(Authenticated)/compose/page.tsx
+++ src/app/(Protected)/(Authenticated)/compose/page.tsx
@@ -4,9 +4,9 @@ const page = () => {
   return (
     <>
       <div className="flex flex-row w-screen h-screen">
-        <div className="flex flex-row w-screen bg-gray-950 p-10 overflow-x-hidden overflow-auto h-screen">
-          <div className="border-2 rounded-xl w-[65%]">left</div>
-          <div className="w-[35%]">right</div>
+        <div className="flex flex-row w-screen bg-white p-10 overflow-x-hidden overflow-auto h-screen">
+          <div className="border-2 rounded-xl w-[65%] bg-gray-200">left</div>
+          <div className="w-[35%] ">right</div>
         </div>
       </div>
     </>
