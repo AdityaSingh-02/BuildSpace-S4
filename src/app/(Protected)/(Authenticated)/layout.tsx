3cdc43a336aea94e20300906a5d8937318b69e4b
Aditya Singh <asaditya2002@gmail.com>
2023-09-03 16:40:14 +0530

    rm: extra imports

--- src/app/(Protected)/(Authenticated)/layout.tsx
+++ src/app/(Protected)/(Authenticated)/layout.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 import React, { useEffect } from "react";
 import UserDataProvider from "@/Context";
-import { useUserData } from "@/Context";
+
 import appwriteService from "@/appwrite-service/config";
 
 export const metadata: Metadata = {
@@ -11,14 +11,6 @@ export const metadata: Metadata = {
 };
 
 export default function dashboard({ children }: { children: React.ReactNode }) {
-  const { userData, setUserData } = useUserData();
-
-  useEffect(() => {
-    appwriteService.getUserData().then((res) => {
-      setUserData({ name: res?.name!, email: res?.email! });
-    });
-  }, []);
-
   return (
     <main>
       <UserDataProvider>{children}</UserDataProvider>
